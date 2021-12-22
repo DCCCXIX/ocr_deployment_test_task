a97bee184c312a1895f88460702be71921ae9250
DCCCXIX <dethcorddemid@gmail.com>
2021-12-22 12:58:38 +0300

    gpu support

--- model_handler.py
+++ model_handler.py
@@ -1,17 +1,26 @@
+import base64
+import io
 import logging
 
 import easyocr
 import torch
+from PIL import Image
 
 
 class Model_Handler:
-    def __init__(self):
+    def __init__(self, langs):
         self.device = torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda:0")
-        logging.info(f"Running on {'CPU' if not torch.cuda.is_available() else torch.cuda.get_device_name()}")
-        self.model = easyocr.Reader(["ru", "en"]).to(self.device)
+        logging.info(f"Running on {self.device}")
+        logging.info(f"Recognition for languages: {langs}")
+
+        self.model = easyocr.Reader(langs, gpu=True if self.device.type == "cuda" else False)
 
     def predict(self, img):
-        return self.model(img)
-
-
-mh = Model_Handler()
+        try:
+            img = base64.b64decode(img)
+            img = Image.open(io.BytesIO(img))
+            result = self.model.readtext(img, detail=0)
+        except Exception:
+            logging.exception("Failed to proccess input")
+            result = []
+        return result
